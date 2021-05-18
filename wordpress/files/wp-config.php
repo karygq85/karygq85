43b6e7350f5bb37f0cac093397916f7c9cd73394
Karina Gomez Q <karygomezq@gmail.com>
2021-05-18 11:28:22 -0500

    change user DB

--- wordpress/files/wp-config.php
+++ wordpress/files/wp-config.php
@@ -23,7 +23,7 @@
 define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'Passw0rd' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', 'Passw0rd' );
